5de9612e5ee69623e8f9c0d0db9784282c2ebb55
Eliyas Hossain <eliyashossain700@gmail.com>
2022-03-25 23:44:33 +0600

    Add To Cart Button Added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsCartPlusFill } from 'react-icons/bs';
 import './Product.css'
 
 const Product = ({product}) => {
@@ -8,9 +9,10 @@ const Product = ({product}) => {
         <div className='product col-12 col-lg-4 my-4'>
             <img className='product-img' src={img} alt="" />
             <div className='product-info'>
-                <h2 className=''>{name}</h2>
-                <h4>$ {price}</h4>
+                <h3 className=''>{name}</h3>
+                <h5>$ {price}</h5>
                 <p>Produced By: <span className='brand'>{brand}</span></p>
+                <button className='add-cart-btn'><p>Add To Cart <BsCartPlusFill size={22}/></p></button>
             </div>
         </div>
     );
