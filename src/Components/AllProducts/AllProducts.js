9d33d1b247b751759b2d3a1952551224d2bce7d7
Eliyas Hossain <eliyashossain700@gmail.com>
2022-03-26 11:36:23 +0600

    Some More Conditions Added

--- src/Components/AllProducts/AllProducts.js
+++ src/Components/AllProducts/AllProducts.js
@@ -12,7 +12,13 @@ const AllProducts = () => {
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+
+    // Add Element To Cart
     const addToCart=(item)=>{
+    if(cart.length>=4){
+        alert('Oops! You Can Only Add 4 Items.')
+    }
+    else{
        const existedItem=cart.find(pro=>pro.id === item.id)
        let newCart=[];
       if(!existedItem){
@@ -27,11 +33,14 @@ const AllProducts = () => {
       }
       setCart(newCart)
     }
+}
+//Reset Cart 
     const cleanCart=()=>{
        let empty=[]
        cart.length>=1 ? setCart(empty) : alert("Oops! There's Nothing To Clean")
         
     }
+//Randomly Choose One Element From Cart
     const chooseOne=()=>{
         const empty=[];
         const luck=cart[Math.floor(Math.random()*cart.length)];
@@ -39,7 +48,7 @@ const AllProducts = () => {
         cart.length>=2 ? setCart(newObj) : alert('Please Select At Least 2 Items!') 
         
     }
-        
+// Delete an Element  
     const deleteItem=(item)=>{
         let newCart=[...cart]
         newCart=cart.filter(pro=>pro.id !== item.id);
@@ -58,9 +67,13 @@ const AllProducts = () => {
                {
                    cart.map(item=><Cart key={item.id} setCart={setCart} deleteItem={deleteItem} item={item}></Cart>)
                }
+               {
+                   cart.length>=1&&<button onClick={cleanCart} className='my-3 mx-auto d-block py-2 px-2 btn-danger clean-cart'>Clean Cart <MdOutlineCleaningServices size={20}/> </button>
+               }
+               {
+                   cart.length>=2&&<button onClick={chooseOne} className='my-3 mx-auto d-block py-2 px-2 btn-success clean-cart'>Select One<GiPerspectiveDiceSixFacesRandom size={22}/></button>
+               }
                
-               <button onClick={cleanCart} className='my-3 mx-auto d-block py-2 px-2 btn-danger clean-cart'>Clean Cart <MdOutlineCleaningServices size={20}/> </button>
-               <button onClick={chooseOne} className='my-3 mx-auto d-block py-2 px-2 btn-success clean-cart'>Select One<GiPerspectiveDiceSixFacesRandom size={22}/></button>
            </div>
         </div>
     );
